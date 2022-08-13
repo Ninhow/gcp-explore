719601089ee9048a1f74070baa133fca51bb0905
Ninhow <allamo.olsson@bonniernews.se>
2022-08-13 12:34:34 +0200

    Changing ports to see if page shows up

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:16.15.1-alpine
 
-EXPOSE 3000
+EXPOSE 8080
 
 WORKDIR /app
 
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 console.log(path.join(__dirname, '/views/index.html'))
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
